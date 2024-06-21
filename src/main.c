4ffc4b37bbc9c4348917a275f85f39b4a1848a8e
Lincoln Cesar Gomes Lima <lincoln.lima@tre-ms.jus.br>
2024-06-21 15:51:58 -0400

    [ADD] adição de funcionalidades no ambiente de teste de funções

--- src/main.c
+++ src/main.c
@@ -13,19 +13,31 @@ int main() {
         printf("Valor: ");
         scanf("%d", &num);
 
-        insere_avl(&arv, num);
+        if(num != 0) insere_avl(&arv, num);
     } while(num != 0);
 
     exibe_avl(&arv);
-    libera_avl(&arv);
+    
+    printf("\n");
 
-//    printf("Sucessor\n");
-//    do {
-//        printf("Valor: ").
-//        scanf("%d", &num);
-//
-//        _sucessor(busca_avl(&arv, num));
-//    } while(num != 0);
+    printf("Busca\n");
+    do {
+        printf("Valor: ");
+        scanf("%d", &num);
+
+        if(num != 0) {
+            Reg * aux = busca_avl(&arv, num);
+
+            while(aux) {
+                printf("item: %d\n", aux->item);
+                aux = aux->prox;
+            }
+        }
+
+        printf("\n");
+    } while(num != 0);
+
+    libera_avl(&arv);
 
     return EXIT_SUCCESS;
 }
