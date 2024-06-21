accc13172b918bd09605fad5a66a4d95e29013b4
Lincoln Cesar Gomes Lima <lincoln.lima@tre-ms.jus.br>
2024-06-21 17:49:22 -0400

    [ADD] estrutura ajustada para registrar Municipios

--- include/avl.h
+++ include/avl.h
@@ -1,9 +1,10 @@
-#ifndef __AVLMOD__
-#define __AVLMOD__
+#ifndef __AVLMODMUN__
+#define __AVLMODMUN__
 #include<stdio.h>
 #include<stdlib.h>
 typedef struct _reg{
-    int item;
+    void * chave;
+    int cod_ibge;
     struct _reg * prox;
 } Reg;
 
@@ -30,11 +31,11 @@ void _rebalancear(Node ** node);
 void _re(Node ** node);
 void _rd(Node ** node);
 /* inserção */
-void insere_avl(Arv * arv, int reg);
-void _insere(Node ** node, Node * pai, int reg);
+void insere_avl(Arv * arv, void * chave, int cod_ibge);
+void _insere(Node ** node, Node * pai, void * chave, int cod_ibge);
 /* busca */
-Reg * busca_avl(Arv * arv, int reg);
-Reg * _busca(Node * node, int reg);
+Reg * busca_avl(Arv * arv, void * reg);
+Reg * _busca(Node * node, void * reg);
 /* destrói */
 void libera_avl(Arv * arv);
 void _libera(Node * node);
